55d9925ac5bbc274704dbe9786e1bf72d0f1a180
JasonHarrer <jazzyeagle79@yahoo.com>
2021-06-29 20:12:23 -0500

    DList.remove_at now successfully passing tests every time

--- dlist.py
+++ dlist.py
@@ -1,29 +1,5 @@
 # ddl.py:  Doubly Linked List implementation
 
-from enum import Enum
-
-#class ResultType(Enum):
-#    OK = 1
-#    Error = 2
-#
-#class Result:
-#    def __init__(self, return_type, value):
-#        self.type = return_type
-#        self.value = value
-#        self.errors = []
-#
-#    def isError(self):
-#        return True if self.type == ResultType.Error else False
-#
-#    def isOK(self):
-#        return True if self.type == ResultType.OK else False
-#
-#    def getErrorMessages(self):
-#        return self.errors
-#
-#    def getValue(self):
-#        return self.value if self.type == ResultType.OK else None
-
 
 class Node:
     def __init__(self, value, next=None, previous=None):
@@ -32,7 +8,16 @@ class Node:
         self.previous = previous
 
     def __str__(self):
-        return f'Node: {self.value}'
+        return_string = f'Node: {self.value}'.ljust(20)
+        if self.previous is None:
+            return_string += '\tPrevious: None'.ljust(20)
+        else:
+            return_string += f'\tPrevious: {self.previous.value}'.ljust(20)
+        if self.next is None:
+            return_string += '\tNext: None'
+        else:
+            return_string += f'\tNext: {self.next.value}'
+        return return_string
 
 
 class DList:
@@ -41,14 +26,14 @@ class DList:
 
 
     def __str__(self):
-        rep = '{ '
+        rep = '{'
         runner = self.head
         while runner is not None:
-            rep += runner.value
+            rep += f'\n\t{runner}'
             if runner.next is not None:
                 rep += ', '
             runner = runner.next
-        rep += ' }'
+        rep += '\n}'
         return rep
 
 
@@ -102,14 +87,34 @@ class DList:
         return self
             
 
-    def remove_at(self, value, pos):
-        pass
-
+    def remove_at(self, pos):
+        if pos >= self.len():
+            print(f'Error: Position {pos} is outside the bounds of the Doubly Linked List.')
+            return self
+        i = 0
+        runner = self.head
+        while i < pos and runner is not None:
+            runner = runner.next
+            i += 1
+        print(f'\ni = {i} of {self.len()-1}, pos = {pos} and runner = {runner}')
+        print(f'\nRemoving Node {runner}')
+        if runner.previous is not None:
+            print('runner.previous is not None')
+            runner.previous.next = runner.next
+        else:
+            self.head = runner.next
+        if runner.next is not None:
+            print('runner.next is not None')
+            runner.next.previous = runner.previous
+        del runner
+        print(f'New DList: {self}')
+        return self
+        
 
     def value(self, pos):
-        if pos > self.len():
+        if pos >= self.len():
             print(f'Error: Position {pos} is outside the bounds of the Doubly Linked List.')
-            return None 
+            return self
         runner = self.head
         counter = 0
         while counter != pos and runner is not None:
@@ -126,11 +131,31 @@ class DList:
             counter += 1
         return counter
 
+
     def remove_duplicates(self):
+        temp_list = []
+        runner = self.head
+        i = 0
+        while runner is not None:
+            if runner.value in temp_list:
+                self.remove_at(i)
+            else:
+                temp_list.append(runner.value)
+                i += 1
+            runner = runner.next
+
+
+    def assign_next_node(self, node):
         pass
+
+
+    def assign_previous_node(self, node):
+        pass
+
 
     def is_circular_list(self):
         pass
+
 
     def reverse(self):
         pass
--- testdlist.py
+++ testdlist.py
@@ -108,5 +108,79 @@ class TestDLL(unittest.TestCase):
             runner = runner.next
 
 
+    def test_remove_at(self):
+        dll = DList()
+        test_nodes = []
+        add_num_nodes = randint(3, 10)
+        remove_num_nodes = randint(1, add_num_nodes)
+        # Add in nodes to test with
+        for i in range(add_num_nodes):
+            name = random_name()
+            test_nodes.append(name)
+            dll.append(name)
+        print('\nNodes Added.')
+        self.assertEqual(dll.len(), len(test_nodes))
+
+        # Now remove the number of nodes
+        while remove_num_nodes > 0:
+            remove_node = randint(0, len(test_nodes)-1)
+            print(f'\nRemoving node #{remove_node} of {len(test_nodes)}/{dll.len()}')
+            del test_nodes[remove_node]
+            dll.remove_at(remove_node)
+            remove_num_nodes -= 1
+            print(f'Assert lengths are equal: {dll.len()} = {len(test_nodes)}')
+            self.assertEqual(dll.len(), len(test_nodes))
+        # Now validate that the two lists are the same
+        self.assertEqual(dll.len(), len(test_nodes))
+        runner = dll.head
+        for i in range(len(test_nodes)):
+            print(f'Assert value {runner.value} = {test_nodes[i]}')
+            self.assertEqual(runner.value, test_nodes[i])
+            if i == 0:
+                self.assertIsNone(runner.previous)
+            else:
+                self.assertEqual(runner.previous.value, test_nodes[i-1])
+            if i == (len(test_nodes)-1):
+                self.assertIsNone(runner.next)
+            else:
+                self.assertEqual(runner.next.value, test_nodes[i+1])
+            runner = runner.next
+
+
+    #def test_remove_duplicates(self):
+    #    dll = DList()
+    #    test_nodes = []
+    #    num_test_nodes = randint(30, 50)  # Really high number to ensure we have duplicates
+    #    # Load test nodes
+    #    for i in range(num_test_nodes):
+    #        name = random_name()
+    #        dll.append(name)
+    #        test_nodes.append(name)
+    #    print('\nNodes added.')
+    #    print('Assert lengths are equal')
+    #    self.assertEqual(dll.len(), len(test_nodes))
+    #    # Now remove duplicates, both from the test_nodes and the DList
+    #    test_nodes = list(dict.fromkeys(test_nodes))
+    #    dll.remove_duplicates()
+
+    #    # Now validate that the two lists are the same
+    #    print('\nNodes removed.')
+    #    print('Assert lengths are equal')
+    #    self.assertEqual(dll.len(), len(test_nodes))
+    #    runner = dll.head
+    #    for i in range(len(test_nodes)):
+    #        self.assertEqual(runner.value, test_nodes[i])
+    #        if i == 0:
+    #            self.assertIsNone(runner.previous)
+    #        else:
+    #            self.assertEqual(runner.previous.value, test_nodes[i-1])
+    #        if i == (len(test_nodes)-1):
+    #            self.assertIsNone(runner.next)
+    #        else:
+    #            self.assertEqual(runner.next.value, test_nodes[i+1])
+    #        runner = runner.next
+
+
+
 if __name__ == '__main__':
     unittest.main()
